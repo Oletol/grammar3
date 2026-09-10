6ddba245c53f8b8e23e0597efe43d87acd102ae1
Oletol <niya@mail.ru>
2026-09-10 20:52:59 +0300

    Connect site to Firebase Realtime Database

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,10 @@
-// Replace null with the exact Firebase web-app configuration object.
-// See firebase-config.example.js and README.md. No credentials are invented here.
-window.FIREBASE_CONFIG = null;
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyALzmljJqZa4_Du4WRBkGEMKmd86IjEii4",
+  authDomain: "grammar3-6f650.firebaseapp.com",
+  databaseURL: "https://grammar3-6f650-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "grammar3-6f650",
+  storageBucket: "grammar3-6f650.firebasestorage.app",
+  messagingSenderId: "1009727888786",
+  appId: "1:1009727888786:web:fbf447a0cf3641e864f54b",
+  measurementId: "G-N3RXQNLG2S"
+};
